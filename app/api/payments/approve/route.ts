95cdf702e70fe8b984a49a69146a44cb8d7c4abd
dustydraft <soyoung@dustydraft.com>
2026-06-16 12:30:08 +0900

    Store PG

--- app/api/payments/approve/route.ts
+++ app/api/payments/approve/route.ts
@@ -1,11 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 
-export async function GET(req: NextRequest) {
-  const searchParams = req.nextUrl.searchParams;
+const resend = new Resend(
+  process.env.RESEND_API_KEY
+);
+
+export async function GET(
+  req: NextRequest
+) {
+
+  const searchParams =
+    req.nextUrl.searchParams;
 
   console.log(
-    Object.fromEntries(searchParams.entries())
+    Object.fromEntries(
+      searchParams.entries()
+    )
   );
+
+  await resend.emails.send({
+    from: "orders@your-domain.com",
+    to: "theplaceyoung@gmail.com",
+
+    subject:
+      "New Order - Exotic Ordinary",
+
+    html: `
+      <h1>New Order</h1>
+      <p>Order Received</p>
+    `,
+  });
 
   return NextResponse.redirect(
     new URL("/success", req.url)
