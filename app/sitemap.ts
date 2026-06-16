d92eabf134fa3fcafd7b694167feaa95d2bde46f
dustydraft <soyoung@dustydraft.com>
2026-06-16 14:26:18 +0900

    SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,18 @@
-export default function sitemap() {
-    return [
-      {
-        url: "https://exoticordinary.com",
-      },
-      {
-        url: "https://exoticordinary.com/store",
-      },
-      {
-        url:
-          "https://exoticordinary.com/store/livings/loofah-white",
-      },
-      {
-        url:
-          "https://exoticordinary.com/store/livings/loofah-pink",
-      },
-      {
-        url:
-          "https://exoticordinary.com/store/livings/loofah-sage",
-      },
-      {
-        url:
-          "https://exoticordinary.com/store/livings/loofah-olive",
-      },
-    ];
-  }
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://exoticordinary.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://exoticordinary.com/store",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://exoticordinary.com/cart",
+      lastModified: new Date(),
+    },
+  ];
+}
